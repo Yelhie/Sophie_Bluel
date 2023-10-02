04d3ee26bc96a19607024c73eb14752603d09958
Yelhie <maelle.ew.m@gmail.com>
2023-10-02 17:56:38 +0200

    Ajout des boutons de filtre
    - Ajout des boutons de filtres au niveau du HTML
    - Ajout du visuel des boutons en CSS
    - début de l'ajout des filtres (Ils n'affichent que les valeur en consol.log pour le moment)

--- projets.js
+++ projets.js
@@ -3,23 +3,59 @@ const projets = await reponse.json();
 
 const sectionGallery = document.querySelector(".gallery");
 
-async function listeProjets(projets) {
-  for (let i = 0; i < projets.length; i++) {
-    const article = projets[i];
+for (let i = 0; i < projets.length; i++) {
+  const article = projets[i];
 
-    const projetElement = document.createElement("figure");
-    const imageElement = document.createElement("img");
-    const titreElement = document.createElement("figcaption");
+  const projetElement = document.createElement("figure");
+  const imageElement = document.createElement("img");
+  const titreElement = document.createElement("figcaption");
 
-    imageElement.src = article.imageUrl;
-    titreElement.innerText = article.title;
+  imageElement.src = article.imageUrl;
+  titreElement.innerText = article.title;
 
-    sectionGallery.appendChild(projetElement);
-    projetElement.appendChild(imageElement);
-    projetElement.appendChild(titreElement);
-  }
+  sectionGallery.appendChild(projetElement);
+  projetElement.appendChild(imageElement);
+  projetElement.appendChild(titreElement);
 }
 
-document.querySelector(".gallery").innerHTML = "";
+//Bouton + Filtre tous
+const boutonTous = document.querySelector(".btn-tous");
 
-listeProjets(projets);
+boutonTous.addEventListener("click", function () {
+  const imagesTous = projets.filter(function (projets) {
+    return projets.categoryId >= 1;
+  });
+  console.log(imagesTous);
+});
+
+//Bouton + Filtre Objets
+const boutonObjets = document.querySelector(".btn-objets");
+
+boutonObjets.addEventListener("click", function () {
+  const imagesObjets = projets.filter(function (projets) {
+    return projets.categoryId == 1;
+  });
+  console.log(imagesObjets);
+});
+
+//Bouton + Filtre Appartements
+const boutonAppartements = document.querySelector(".btn-appartements");
+
+boutonAppartements.addEventListener("click", function () {
+  const imagesAppartements = projets.filter(function (projets) {
+    return projets.categoryId == 2;
+  });
+  console.log(imagesAppartements);
+});
+
+//Bouton + Filtre Hotels & restaurants
+const boutonHotelsRestaurants = document.querySelector(
+  ".btn-hotelsrestaurants"
+);
+
+boutonHotelsRestaurants.addEventListener("click", function () {
+  const imagesHotelsRestaurants = projets.filter(function (projets) {
+    return projets.categoryId == 3;
+  });
+  console.log(imagesHotelsRestaurants);
+});
